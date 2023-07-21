9aceba5154920a819d0b84212ff108a1bafd3ed8
CamposDJoel <140183065+CamposDJoel@users.noreply.github.com>
2023-07-21 11:13:34 -0400

    Pokemon Object Class Creation
    Class create with a tmp data set and constructor

---
+++ PokemonKanto/Pokemon.cs
@@ -0,0 +1,26 @@
+﻿//Joel Campos
+//7/21/2023
+//Pokemon Object Class
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonKanto
+{
+    public class Pokemon
+    {
+        public Pokemon()
+        {
+            _id = 0;
+            _name = "tmp";
+            _nickname = "tmp";
+        }
+
+        private int _id;
+        private string _name;
+        private string _nickname;
+    }
+}
